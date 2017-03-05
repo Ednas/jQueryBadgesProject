1e052288b6a10a9b3e021338f38b8782141d2bc1
Ednas <edna@edna.tech>
2017-03-05 02:23:13 -0500

    Refactoring, taking out commented out code, making css file more clear

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,8 +7,7 @@ $.ajax({
 		$('#user').append(
 			'<h2>'+ response.user.username.charAt(0).toUpperCase() + response.user.username.slice(1) +'</h2>'
 			+ '<img src=' + response.user.avatar + '>'
-			+ '<h4>Has earned: ' + response.user.total_score + ' points</h4>'
-			);
+			+ '<h4>Has earned: ' + response.user.total_score + ' points</h4>');
 		//Information about badges earned
    for (var i = 0; i < response.courses.completed.length; i++) {
    	$('#badges').append('<div class="course"> <h3>' + response.courses.completed[i].title 
@@ -16,7 +15,7 @@ $.ajax({
    		+ '<a class="btn btn-primary" href=' + response.courses.completed[i].url + ' target="_blank">See Course</a>'
    		+ '</h3>'
    		+'</div>');
-   };
+	   };
    		//What I'm tinkering with
    	for (var i = 0; i < response.courses.in_progress.length; i++) {
    	$('#inProgress').append('<div class="course"> <h3>' + response.courses.in_progress[i].title 
@@ -24,23 +23,15 @@ $.ajax({
    		+ '<a class="btn btn-primary" href=' + response.courses.in_progress[i].url + ' target="_blank">See Course</a>'
    		+ '</h3>'
    		+'</div>');   		
-   	};
+	   	};
    		//Badges earned
    	for (var i = 0; i < response.badges.length; i++) {
    	$('#badge').append(
    		'<div class="course"> <h3>' + response.badges[i].name 
    		+ '<img src=' + response.badges[i].badge + '>'
-   		//+ '<a class="btn btn-primary" href=' + response.badges.[i].url + ' target="_blank">See Course</a>'
    		+ '</h3>'
    		+'</div>');   		
-
-   	};
-
-        //console.log(response.courses);
-        //console.log(response);
+	   	};
     }
   });
-
-  });
-
-//});
+});
